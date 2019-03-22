694e6ececf6614605c4d5606a01ad1ffa907b05d
MxABC <lbxia20091227@gmail.com>
2019-03-22 09:04:28 +0800

    demo删除么有使用的权限代码

--- LBXScanDemo/LBXScanDemo/LBXPermissions/LBXPermissionSetting.h
+++ LBXScanDemo/LBXScanDemo/LBXPermissions/LBXPermissionSetting.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
+
+API_AVAILABLE(ios(8.0)) 
 @interface LBXPermissionSetting : NSObject
 
 #pragma mark- guide user to show App privacy setting
@@ -30,5 +33,21 @@
                                              msg:(NSString*)message
                                           cancel:(NSString*)cancel
                                          setting:(NSString*)setting;
+
+
+/**
+ show dialog to guide user to show App privacy setting
+ 
+ @param title title
+ @param message privacy message
+ @param cancel cancel button text
+ @param setting setting button text,if user tap this button ,will show App privacy setting
+ @param completion user has been choosed
+ */
++ (void)showAlertToDislayPrivacySettingWithTitle:(NSString*)title
+                                             msg:(NSString*)message
+                                          cancel:(NSString*)cancel
+                                         setting:(NSString*)setting
+                                      completion:(void(^)(void))completion;
 
 @end
