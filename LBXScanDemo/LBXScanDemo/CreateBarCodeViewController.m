4b2a4943abbe600a0cb33a532534471531ac3c8b
Jadyn <>
2020-09-24 16:42:47 +0800

    更新二维码和条形码同时可扫描

--- LBXScanDemo/LBXScanDemo/CreateBarCodeViewController.m
+++ LBXScanDemo/LBXScanDemo/CreateBarCodeViewController.m
@@ -74,6 +74,7 @@
     [view addSubview:_qrImgView];
     self.qrView = view;
     
+    NSLog(@"设置条形码和二维码同时可扫描");
     
     //条形码
     self.tView = [[UIView alloc]initWithFrame:CGRectMake( (CGRectGetWidth(self.view.frame)-CGRectGetWidth(self.view.frame)*5/6)/2,
